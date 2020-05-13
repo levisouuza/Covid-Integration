bd9936509028b2b072dfddbc55dca5109da92bf1
levis <levisouuza@gmail.com>
2020-05-13 08:32:39 -0300

    update scripts

--- DataCovidWorld/main.py
+++ DataCovidWorld/main.py
@@ -1,38 +1,65 @@
 
-from Scripts.CovidWorld import Extract, Transform
+from Scripts.DataCovidWorld import Extract, Transform
 import os
 import time
 import re
 from datetime import datetime, timedelta
+import schedule
 
 dir_initial = r'C:\Users\levis\Documents\Covid'
 format_dif = list()
 
 dt_ini = datetime.now().date() - timedelta(days=5)
-dt_ini.strftime('%m-%d-%Y')
+dt = dt_ini.strftime('%m-%d-%Y')
 
 
-for dates in Extract.gera_datas(dt):
-    if os.path.exists(os.path.join(dir_initial,f'{dates}.csv')):
-        os.remove(os.path.join(dir_initial,f'{dates}.csv'))
 
-time.sleep(1)
+def world_covid(directory):
 
-Extract.extract_git(dir_initial,dt)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-time.sleep(2)
+    for dates in Extract.gera_datas(dt):
+        if os.path.exists(os.path.join(directory,f'{dates}.csv')):
+            os.remove(os.path.join(directory,f'{dates}.csv'))
 
-if os.path.exists(dir_initial):
-    for arq in os.listdir(dir_initial):
+    time.sleep(1)
 
-        # Condição que verifica o formato dos arquivo e direciona para sua respectiva estrutura de dados.
-        if re.search('csv', arq):
+    Extract.extract_git(directory,dt)
 
-            if arq[:2] == '03' and arq[3:5] >= '22' and arq[6:10] == '2020':
-                format_dif.append(arq)
-            elif arq[:2] >= '04' and arq[6:10] >= '2020':
-                format_dif.append(arq)
+    time.sleep(2)
 
-    # transformação e carga no diretório master dos arquivos com formato distinto.
-    Transform.trans_arquivos(dir_initial, format_dif, dir_initial)
+    if os.path.exists(directory):
+        for arq in os.listdir(directory):
 
+            # Condição que verifica o formato dos arquivo e direciona para sua respectiva estrutura de dados.
+            if re.search('csv', arq):
+
+                if arq[:2] == '03' and arq[3:5] >= '22' and arq[6:10] == '2020':
+                    format_dif.append(arq)
+                elif arq[:2] >= '04' and arq[6:10] >= '2020':
+                    format_dif.append(arq)
+
+        # transformação e carga no diretório master dos arquivos com formato distinto.
+        Transform.trans_arquivos(directory, format_dif, directory)
+
+
+def main():
+
+    world_covid(dir_initial)
+
+    time.sleep(5)
+
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    print(f'Task finalizada às {now} com sucesso.')
+
+#schedule.every(1).day.at('00:00').do(main)
+schedule.every(1).day.at('08:25').do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
+
+main()
+
+exit()
