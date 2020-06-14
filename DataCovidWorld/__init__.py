b6a0c69785e087d6b9c7e002f190ac5aac4e08f2
levi souza <levisouuza@gmail.com>
2020-06-14 16:12:17 -0300

    atualizando e criando novas features

--- DataCovidWorld/__init__.py
+++ DataCovidWorld/__init__.py
@@ -1,2 +1,6 @@
 # __init__.py
 # -*- coding: UTF-8 -*-
+
+import extract
+import transform
+import del_arq
--- DataCovidWorld/main.py
+++ DataCovidWorld/main.py
@@ -2,96 +2,96 @@
 # __init__.py
 # -*- coding: UTF-8 -*-
 
-import os
-import time
-import re
 from datetime import datetime, timedelta
-import schedule
+from database import load_db
 import pandas as pd
-import Extract
-import Transform
-import Delete
+import transform
+import schedule
+import requests
+import extract
+import del_arq
+import time
+import os
+import re
 
 
 dir_inicial = '/home/levis/covidWorld'
+url = 'http://localhost:15432/'
+
 format_dif = list()
 
-dt_ini = datetime.now().date() - timedelta(days=3)
+dt_ini = datetime.now().date() - timedelta(days=2)
 dt = dt_ini.strftime('%m-%d-%Y')
 
 carga_inicial = 'S'
 
+def world_covid(directory, url):
 
-def world_covid(directory):
+    try:
+        #criando diretório para persistir os arquivos csv.
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    #criando diretório para persistir os arquivos csv.
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        #excluindo arquivos caso existam.
+        for dates in extract.gera_datas(dt):
+            if os.path.exists(os.path.join(directory,f'{dates}.csv')):
+                os.remove(os.path.join(directory,f'{dates}.csv'))
 
-    #excluindo arquivos caso existam.
-    for dates in Extract.gera_datas(dt):
-        if os.path.exists(os.path.join(directory,f'{dates}.csv')):
-            os.remove(os.path.join(directory,f'{dates}.csv'))
+        time.sleep(1)
 
-    time.sleep(1)
+        #método responsável por criar requests do repositório John Hopkins
+        extract.extract_git(directory, dt)
 
-    #método responsável por criar requests do repositório John Hopkins
-    Extract.extract_git(directory, dt)
+        time.sleep(2)
 
-    time.sleep(2)
+        #verificando arquivos que estão em formatos diferentes e padronizando-os para um único formato.
+        if os.path.exists(directory):
+            for arq in os.listdir(directory):
 
-    #verificando arquivos que estão em formatos diferentes e padronizando-os para um único formato.
-    if os.path.exists(directory):
-        for arq in os.listdir(directory):
+                # Condição que verifica o formato dos arquivo e direciona para sua respectiva estrutura de dados.
+                if re.search('csv', arq):
 
-            # Condição que verifica o formato dos arquivo e direciona para sua respectiva estrutura de dados.
-            if re.search('csv', arq):
+                    if arq[:2] == '03' and arq[3:5] >= '22' and arq[6:10] == '2020':
+                        format_dif.append(arq)
+                    elif arq[:2] >= '04' and arq[6:10] >= '2020':
+                        format_dif.append(arq)
 
-                if arq[:2] == '03' and arq[3:5] >= '22' and arq[6:10] == '2020':
-                    format_dif.append(arq)
-                elif arq[:2] >= '04' and arq[6:10] >= '2020':
-                    format_dif.append(arq)
+            #método para transformação das features
+            transform.trans_arquivos(directory, format_dif, directory)
 
-        #método para transformação das features
-        Transform.trans_arquivos(directory, format_dif, directory)
+        time.sleep(1)
 
-    time.sleep(1)
+        #método que irá criar a data das ocorrências dos datasets
+        transform.feature_date(dir_inicial)
 
-    #método que irá criar a data das ocorrências dos datasets
-    Transform.feature_date(dir_inicial)
+        time.sleep(2)
 
-    time.sleep(2)
+        #criação dataset full
 
-    #criação dataset full
+        if carga_inicial == 'S':
+            transform.load_dataset(dir_inicial, carga_inicial)
 
-    if carga_inicial == 'S':
-        Transform.load_dataset(dir_inicial, carga_inicial)
+        elif carga_inicial == 'N':
+            transform.load_dataset(dir_inicial, carga_inicial, extract.gera_datas(dt))
 
-    elif carga_inicial == 'N':
-        Transform.load_dataset(dir_inicial, carga_inicial, Extract.gera_datas(dt))
+        time.sleep(2)
 
-    time.sleep(2)
+        del_arq.del_arq(dir_inicial)
 
-    Delete.del_arq(dir_inicial)
+        try:
 
-'''def main():
+            requests.get(url).status_code
 
-    world_covid(dir_initial)
+            try:
+                load_db.load_database('localhost', 'postgres', 'postgres', '1123581321', carga_inicial, dir_inicial, extract.gera_datas(dt))
+            except:
+                print('Erro de inclusão no banco.')
 
-    time.sleep(5)
+        except:
+            print('Falha na conexão.')
 
-    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    print(f'Task finalizada às {now} com sucesso.')
+    except KeyboardInterrupt:
+        print('Execução cancelada pelo usuário.')
 
-#schedule.every(1).day.at('00:00').do(main)
-schedule.every(1).day.at('20:43').do(main)
 
-while True:
-    schedule.run_pending()
-    time.sleep(30)
-
-main()
-
-exit()'''
-
-world_covid(dir_inicial)
+world_covid(dir_inicial, url)
